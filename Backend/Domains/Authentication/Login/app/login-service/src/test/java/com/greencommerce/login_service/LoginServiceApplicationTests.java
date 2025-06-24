c498f25c97e18d60e2808c0d30ccf2b04dd8a1ea
Duvard <duvardesteban@hotmail.com>
2025-06-24 15:34:05 -0500

    test: :rocket: Test Deploy ec2 #4

--- Backend/Domains/Authentication/Login/app/login-service/src/test/java/com/greencommerce/login_service/LoginServiceApplicationTests.java
+++ Backend/Domains/Authentication/Login/app/login-service/src/test/java/com/greencommerce/login_service/LoginServiceApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LoginServiceApplicationTests {
 
+	// This test checks if the Spring application context loads successfully.
 	@Test
 	void contextLoads() {
 	}
