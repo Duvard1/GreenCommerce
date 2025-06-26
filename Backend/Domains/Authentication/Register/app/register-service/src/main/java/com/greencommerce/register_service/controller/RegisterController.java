dabefe012afd4ca87bd40d6f440a7f730da3d612
Duvard <duvardesteban@hotmail.com>
2025-06-26 00:07:50 -0500

    fix: :bug: Update workflow deployment #8

--- Backend/Domains/Authentication/Register/app/register-service/src/main/java/com/greencommerce/register_service/controller/RegisterController.java
+++ Backend/Domains/Authentication/Register/app/register-service/src/main/java/com/greencommerce/register_service/controller/RegisterController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class RegisterController {
     @Autowired
-    // RegisterService is a service that handles user registration logic
     private RegisterService registerService;
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody User user) {
