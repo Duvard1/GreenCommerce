d9df91908e9362c99cd5197fd5574d8927c6d8b4
Duvard <duvardesteban@hotmail.com>
2025-07-10 13:14:50 -0500

    fix: :bug: Fix decode token function

--- Backend/Domains/Users/UpdateUser/app/utils/jwt_utils.py
+++ Backend/Domains/Users/UpdateUser/app/utils/jwt_utils.py
@@ -3,16 +3,13 @@ import jwt
 from jwt import DecodeError, ExpiredSignatureError
 from app.config.settings import settings
 
-# Ejemplo de cómo decodificar el token en el microservicio
 def decode_token(auth_header: str) -> dict:
     if not auth_header.startswith("Bearer "):
         raise HTTPException(status_code=401, detail="Token not provided")
 
     token = auth_header.split(" ")[1]
     try:
-        secret = "test-secret"  # Asegúrate de que coincida con tu configuración real
-        payload = jwt.decode(token, secret, algorithms=["HS256"])
-        return payload
+        return jwt.decode(token, settings.JWT_SECRET, algorithms=["HS256"])
     except ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="Expired token")
     except DecodeError:
