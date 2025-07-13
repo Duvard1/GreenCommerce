ff39b700aca8ad68bfec8f38b423a11a0a1c2151
Duvard <duvardesteban@hotmail.com>
2025-07-13 17:18:03 -0500

    test: :white_check_mark: Deploy uploadImage microservice

--- Frontend/green-commerce/src/lib/api/endpoints.ts
+++ Frontend/green-commerce/src/lib/api/endpoints.ts
@@ -1,10 +1,10 @@
 const BASE_URL_REGISTER = process.env.NEXT_PUBLIC_API_REGISTER_URL || 'http://44.193.255.85:8081';
-const BASE_URL_LOGIN = process.env.NEXT_PUBLIC_API_LOGIN_URL || 'http://44.193.255.85:8082';
-const BASE_URL_USER = process.env.NEXT_PUBLIC_API_USER_URL || 'http://3.216.196.163:8081';
-const BASE_URL_PROFILE = process.env.NEXT_PUBLIC_API_PROFILE_URL || 'http://3.216.196.163:8082';
+const BASE_URL_LOGIN = process.env.NEXT_PUBLIC_API_LOGIN_URL || 'http://13.219.106.149';
+const BASE_URL_USER = process.env.NEXT_PUBLIC_API_USER_URL || 'http://13.219.106.149';
+const BASE_URL_PROFILE = process.env.NEXT_PUBLIC_API_PROFILE_URL || 'http://13.219.106.149';
 const BASE_URL_DELETE = process.env.NEXT_PUBLIC_API_DELETE_URL || 'http://3.216.196.163:8003';
-const BASE_URL_PRODUCT = process.env.NEXT_PUBLIC_API_PRODUCT_URL || 'http://52.54.233.190:3008';
-const BASE_URL_GRAPHQL = process.env.NEXT_PUBLIC_API_GRAPHQL_URL || 'http://52.54.233.190:3000';
+const BASE_URL_PRODUCT = process.env.NEXT_PUBLIC_API_PRODUCT_URL || 'http://13.219.106.149';
+const BASE_URL_GRAPHQL = process.env.NEXT_PUBLIC_API_GRAPHQL_URL || 'http://13.219.106.149';
 
 export const ENDPOINTS = {
   AUTH: {
